58c65bbc30177942012e42bc2ec59e407cde461b
Dustin Fisher <49411538+Red-Singularity@users.noreply.github.com>
2022-10-15 04:00:23 -0400

    Determined Input File Style
    Determined that I will use two separate .dat files as inputs for the simulator. Created a python file that outputs a single dimensional array to dat files in proper format. Also made concatanation function to combine separate PIL images of same width together

--- _Dustin/testbed/V1/snowboard_sim_v1.py
+++ _Dustin/testbed/V1/snowboard_sim_v1.py
@@ -7,11 +7,48 @@
 # this has the potential to be the start of a visual representation in a custom
 
 from PIL import Image
+import fileinput
+from numpy import loadtxt
 
-sim = Image.new( 'RGB', (20,114), "black") # Create a new black image that has the size of ideal snowboard image
-pixels = sim.load() # Create the pixel map
-for i in range(sim.size[0]):    # For every pixel:
-     for j in range(sim.size[1]):
-        pixels[i,j] = (i, j, 100) # Set the colour accordingly
+array_size = 1140 + 1
 
-sim.show()
+def get_input_one():
+        """open and read .dat files and convert to integer arrays"""
+        Half_One = open("Half_One.dat", "r")
+        Board_Half_One = loadtxt("Half_One.dat", delimiter=",",unpack=False)
+        Board_Half_One = Board_Half_One.astype(int)
+        return Board_Half_One
+
+def get_input_two():
+        """open and read .dat files and convert to integer arrays"""
+        Half_Two = open("Half_Two.dat", "r")
+        Board_Half_Two = loadtxt("Half_Two.dat", delimiter=",",unpack=False)
+        Board_Half_Two = Board_Half_Two.astype(int)
+        return Board_Half_Two
+
+def create_image():
+        Board_Half_One = get_input_one()
+        Board_Half_Two = get_input_two()
+
+        Image1 = Image.new( 'RGB', (114,10), 0x000000) # mode, size, color
+        pixels1 = Image1.load() # Create the pixel map
+        for i in range(Image1.size[0]):
+                for j in range(Image1.size[1]):
+                        pixels1[i,j] = (i, j, Board_Half_One[i])
+        Image1.show()
+
+        Image2 = Image.new( 'RGB', (114,10), 0x000000) # mode, size, color
+        pixels1 = Image2.load() # Create the pixel map
+
+        Image3 = concatonate(Image1, Image2)
+        Image3.show()
+
+def concatonate(im1, im2):
+        """Concatonates two images on top of each other to create one image"""
+        dst = Image.new('RGB', (im1.width, im1.height + im2.height))
+        dst.paste(im1, (0, 0))
+        dst.paste(im2, (0, im1.height))
+        return dst
+
+create_image()
+
